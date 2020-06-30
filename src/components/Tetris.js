c399a59e35b16074c358dd484a29fb267e91ece1
Abiel Hailemariam <abielh06@gmail.com>
2020-06-30 14:01:14 -0500

    stage setup and tetris block functions

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-// import { createStage } from '../gameHelpers'
+import { createStage } from '../gameHelpers'
 // Styled components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 // custom hooks
@@ -11,17 +11,46 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
-    console.log('re-render');
+    
 
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
-    const [player] = usePlayer();
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
     const [stage, setStage] = useStage(player);
 
+    console.log('re-render');
+
+    const movePlayer = dir => {
+        updatePlayerPos({ x: dir, y: 0 });
+
+    }
+    const startGame = () => {
+        // reset
+        setStage(createStage());
+        resetPlayer();
+    }
+    const drop = () => {
+        updatePlayerPos({ x:0, y:1, collided: false })
+    }
+    const dropPlayer = () =>{
+        drop();
+    }
+    const move = ({ keyCode }) =>{
+        if(!gameOver){
+            if(keyCode===37){
+                movePlayer(-1);
+            }else if(keyCode === 39){
+                movePlayer(1)
+            }else if(keyCode === 40){
+                dropPlayer();
+            }
+        }
+
+    }
 
 
     return (
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
             <StyledTetris>
             <Stage stage= {stage} />
             <aside>
@@ -34,7 +63,7 @@ const Tetris = () => {
                 <Display text="Level" />
                 </div>
                 )}
-                <StartButton/>
+                <StartButton callback={startGame}/>
             </aside>
             </StyledTetris>
 
